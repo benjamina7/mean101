273eecb7b9b5e6b6505d94e57f96bfea56a35dc4
Ben Alpren <techben@live.com>
2015-09-23 17:08:30 -0400

    working with Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	stylus 	= require('stylus'),
 	logger 	= require('morgan'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -26,6 +27,24 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+// use mongoDB:connection string:    /myDBName   <-- this will be created if it does not already exist
+mongoose.connect('mongodb://localhost/mean101');
+var db = mongoose.connection;
+// listen for some stuff in the databse:
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+	console.log('mean101 db opened');
+});
+
+var messageSchema =  mongoose.Schema({ message: String });
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message
+	.findOne() // return very first document in the collection
+	.exec(function(err, messageDoc) { // callback function
+		mongoMessage = messageDoc.message;
+	});
+
 // Partials route - any url starting with "partial", then take in the next part of the url as a variable named partialPath
 app.get('/partials/:partialPath', function(req, resp) { 
 	var partialPath = req.params.partialPath;
@@ -39,7 +58,9 @@ app.get('/partials/:partialPath', function(req, resp) {
 
 // add a route to deliver our index page (a 'catch-all' route)
 app.get('*', function(req, resp) { // all routes from a server perspective will show the index page (client side routing can pick it up from there)
-	resp.render('index');
+	resp.render('index', {
+		mongoMessage: mongoMessage
+	});
 }); 
 
 var port = 3030;
